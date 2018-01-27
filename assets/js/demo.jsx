1e6ff79b0a05b1593c06e4b754b81a0edae424a7
David Celentano <davidwcelentano@gmail.com>
2018-01-27 15:03:33 -0500

    basic matching fun

--- assets/js/demo.jsx
+++ assets/js/demo.jsx
@@ -9,23 +9,37 @@ export default function run_demo(root) {
 class Demo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { moves: 0, tiles:['a', 'b', 'a', 'b', 'c', 'd', 'c', 'd', 'e', 'f', 'e', 'f', 'g', 'h', 'g', 'h']}; //TODO RANDOM
+    this.state = { moves: 0, tiles:['a', 'b', 'a', 'b', 'c', 'd', 'c', 'd', 'e', 'f', 'e', 'f', 'g', 'h', 'g', 'h'], matches: [], guess: 'a'}; //TODO RANDOM
   }
 
   increment(moves) {
     var moves = 1 + this.state.moves;
     this.setState({moves: moves});
   }
+  
+  guess() {
+   // console.log("guess made");
+    var value = 'a'
+    if (this.state.guess == '1') {
+      this.setState({guess: value});
+    }
+    else {
+      if (this.state.guess == value) {
+     //   console.log(value);
+        var nMatches = this.state.matches;
+        nMatches.push(value);
+        this.setState({matches: nMatches});
+      }
+    }
+  }
 
   render() {
-    var increment = this.increment.bind(this);
+    var guess = this.guess.bind(this);
+   // console.log(this.state.matches);
     return (
       <div className="row">
         <div className="col">
-	  <Side show={this.state.moves == 0} increment={increment} value={this.state.tiles[0]} />
-	  <Side show={this.state.moves == 0} increment={increment} value={this.state.tiles[1]} />
-	  <Side show={this.state.moves == 0} increment={increment} value={this.state.tiles[2]} />
-	  <Side show={this.state.moves == 0} increment={increment} value={this.state.tiles[3]} />
+	  <Side state={this.state} matches={this.state.matches} guess={guess} value={this.state.tiles[0]} />
         </div>
       </div>
     );
@@ -33,17 +47,19 @@ class Demo extends React.Component {
 }
 
 function Side(params) {
-  if (params.show) {
+  if (params.matches.includes(params.value)) {
+    console.log("showing letter");
     return (
-      <div id="side-0" className="side col" onClick={() => params.increment()} >
+      <div id="side-0" className="side col">
         <Button>{params.value}</Button>
       </div>
     );
   }
   else {
+    console.log("showing ?");
     return (
       <div id="side-0" className="side col">
-	<p>{params.moves}</p>
+	<Button onClick={() => params.guess()}>?</Button>
       </div>
     );
   }
