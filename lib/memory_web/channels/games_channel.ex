62b7406f6c716156b94882dbb57cadad36482795
David Celentano <davidwcelentano@gmail.com>
2018-02-07 16:58:21 -0500

    basic page layout added

--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -1,7 +1,7 @@
 defmodule MemoryWeb.GamesChannel do
   use MemoryWeb, :channel
 
-  def join("games:" <> name, payload, socket) do
+  def join("game:" <> name, payload, socket) do
     if authorized?(payload) do
       {:ok, socket}
     else
--- lib/memory_web/controllers/page_controller.ex
+++ lib/memory_web/controllers/page_controller.ex
@@ -4,4 +4,9 @@ defmodule MemoryWeb.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def game(conn, params) do
+    game_name = params["game_name"]
+    render conn, "game.html", game_name: game_name
+  end
 end
